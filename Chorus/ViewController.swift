3dc0fbfc59d5a7ec89f8f466f61184fb8d87856a
Hao Guan <haohowardguan@gmail.com>
2018-08-19 23:43:45 -0700

    Initial Commit

---
+++ Chorus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chorus
+//
+//  Created by Hao Guan on 8/19/18.
+//  Copyright © 2018 Hao Guan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
